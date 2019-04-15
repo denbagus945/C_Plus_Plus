e314d57c70cbf58038729a4d7d77f64123dfb81d
denbagus945 <48904594+denbagus945@users.noreply.github.com>
2019-04-15 09:34:39 +0700

    Add files via upload

---
+++ Rumus Luas Segitiga.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //untuk variabel
+    float alas;
+    float tinggi;
+    
+    //input alas & tinggi
+    cout<<"Masukan alasnya = ";
+    cin>>alas;
+    
+    cout<<"Masukkan tingginya = ";
+    cin>>tinggi;
+    
+    //output hasil
+    cout<<"Jadi luas segitigaya adalah "<<0.5*alas*tinggi<<endl;
+    
+    
+
+    return 0;
+}
